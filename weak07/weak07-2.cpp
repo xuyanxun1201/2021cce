7c671c9b3a37aaedf66576513fff098b0d757b7f
xuyanxun <91170832+xuyanxun1201@users.noreply.github.com>
2021-10-27 12:01:01 +0800

    Add files via upload

---
+++ weak07/weak07-2.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    int n=10;
+//  if(n>0) printf("n大於0\n");
+    while(n>0) printf("n大於0\n");
+}
---
+++ weak07/weak07-3.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int n=2;
+    while(n>0) {
+        printf("n¤j©ó0\n");
+        n=n-1;
+    }
+}
