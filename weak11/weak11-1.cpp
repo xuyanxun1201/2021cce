be5a81e8d6b7c56d051b20b98df129cc1ef2da4b
xuyanxun <91170832+xuyanxun1201@users.noreply.github.com>
2021-11-24 11:47:33 +0800

    Add files via upload

---
+++ weak11/weak11-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int a[10000000];
+int main()
+{
+    int m;
+    scanf("%d",&m);
+    for(int i=2;i<m;i++){
+        if(a[i]==0){
+            printf("%d ",i);
+            for(int k=i+i;k<m;k=k+i){
+                a[k]=1;
+            }
+        }
+    }
+}
